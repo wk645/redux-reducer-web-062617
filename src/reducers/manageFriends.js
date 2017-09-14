7176f50ecc0eda29a96854a1ad85a6b584f2a124
wk645 <wk645@nyu.edu>
2017-09-13 22:35:48 -0400

    reducer

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+	switch (action.type) {
+		case 'ADD_FRIEND':
+			return Object.assign({}, {friends: [...state.friends, action.friend]})
+		case 'REMOVE_FRIEND':
+			return ({...state, friends: state.friends.filter(friend => friend.id != action.id)})
+		default:
+			return state
+	}
 }
+
+// { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Joe', hometown: 'Boston', id: 101 }, { name: 'Steven', hometown: 'Philadephia', id: 102 } ] } to equal { friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Steven', hometown: 'Philadephia', id: 102 } ] }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,19 @@
 export function managePresents(state, action){
+	switch (action.type) {
+		case 'INCREASE':
+			return {numberOfPresents: state.numberOfPresents + 1}
+		default: 
+			return state
+	}
 }
+
+
+// Create copies of obj using Object.assign() with new and updated values
+// let dog = {id: 1, name: 'scooby', color: 'brown', age: 4}
+// let olderDog = Object.assign({}, dog, {age: dog.age + 1})
+// start with a new empty obj, copy over original dog, and overwrite the 
+// age property with a new value of 5
+// USE spread syntax to copy properties
+// let olderDog = {...dog, age: dog.age + 1}
+// return a new obj that contais all key-value pairs from dog copied over 
+// with the age key overwritten with a new value
